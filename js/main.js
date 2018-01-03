0036251ce3005a758feef85dfdb19c390a61e87e
qiuxan <qiuxan@gmail.com>
2018-01-03 23:58:11 +1100

    add animation plug in

--- js/main.js
+++ js/main.js
@@ -151,7 +151,23 @@ $('.team-carousel').carouFredSel({
 	
 	
 // 	end of testimonials code
-	
+
+	$('.animation').each(function(){
+		var waypoint = new Waypoint({
+		  element: this,
+		  handler: function(direction) {
+			  
+		    var animation = $(this.element).attr('data-animation');
+			$(this.element).css('opacity','1');
+		    $(this.element).addClass("animated " + animation);//there is a space after the add calss animated
+		  
+		  },
+		  offset: "75%" 
+		})		
+		
+	});
+
+
 	
 });
 
